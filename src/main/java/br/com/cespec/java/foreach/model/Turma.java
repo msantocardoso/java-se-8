38273385d2e5d5e5bfd40b10247456170f5a9965
msantocardoso@gmail.com <msantocardoso@gmail.com>
2015-04-11 18:08:28 -0300

    implementacoes stream

--- src/main/java/br/com/cespec/java/foreach/model/Turma.java
+++ src/main/java/br/com/cespec/java/foreach/model/Turma.java
@@ -1,5 +1,7 @@
 package br.com.cespec.java.foreach.model;
 
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.partitioningBy;
 import static java.util.stream.Collectors.toList;
 
 import java.util.ArrayList;
@@ -7,8 +9,12 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 public class Turma {
+	private static final Predicate<Aluno> ALUNO_APROVADO = aluno -> aluno.getNota() >= 7.0;
 
 	private List<Aluno> alunos = new ArrayList<>();
 
@@ -28,5 +34,25 @@ public class Turma {
 
 	public DoubleSummaryStatistics getSumarizacaoDasNotas() {
 		return alunos.stream().mapToDouble(Aluno::getNota).summaryStatistics();
+	}
+
+	public Map<Sexo, List<Aluno>> getAlunosAgrupadoPorSexo() {
+		return alunos.stream().collect(groupingBy(Aluno::getSexo));
+	}
+
+	public Map<Boolean, List<Aluno>> particionarAlunosAprovadosEReprovados() {
+		return alunos.stream().collect(partitioningBy(ALUNO_APROVADO));
+	}
+
+	public boolean existeAlunoAprovado() {
+		return alunos.stream().anyMatch(ALUNO_APROVADO);
+	}
+
+	public boolean todosEstaoAprovados() {
+		return alunos.stream().allMatch(ALUNO_APROVADO);
+	}
+
+	public Optional<Double> somarTodasAsNotas() {
+		return alunos.stream().map(Aluno::getNota).reduce((a, b) -> a + b);
 	}
 }
---
+++ src/test/java/br/com/cespec/java/stream/TurmaTest.java
@@ -0,0 +1,82 @@
+package br.com.cespec.java.stream;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.cespec.java.foreach.model.Aluno;
+import br.com.cespec.java.foreach.model.Sexo;
+import br.com.cespec.java.foreach.model.Turma;
+
+public class TurmaTest {
+
+	private Turma turma = new Turma();
+
+	@Before
+	public void setUpBeforeClass() {
+		turma.adicionar(new Aluno("Murilo", Sexo.MASCULINO, 8.9) );
+		turma.adicionar(new Aluno("Teste I", Sexo.FEMININO, 9.0));
+		turma.adicionar(new Aluno("Fanfarrão", Sexo.MASCULINO, 10.0));
+	}
+
+	@Test
+	public void deveRetornarOsAlunosAprovados() {
+		List<Aluno> aprovados = turma.getAprovados();
+
+		assertEquals(3, aprovados.size());
+	}
+
+	@Test
+	public void deveRetornarSumarizacaoDasNotas() {
+		DoubleSummaryStatistics sumario = turma.getSumarizacaoDasNotas();
+
+		assertEquals(Double.valueOf(9.299999999999999), Double.valueOf(sumario.getAverage()));
+		assertEquals(Double.valueOf(3), Double.valueOf(sumario.getCount()));
+		assertEquals(Double.valueOf(8.9), Double.valueOf(sumario.getMin()));
+		assertEquals(Double.valueOf(10.0), Double.valueOf(sumario.getMax()));
+		assertEquals(Double.valueOf(27.9), Double.valueOf(sumario.getSum()));
+	}
+
+	@Test
+	public void deveAgruparAlunosPorSexo() {
+		Map<Sexo, List<Aluno>> mapAlunos = turma.getAlunosAgrupadoPorSexo();
+
+		assertEquals(2, mapAlunos.get(Sexo.MASCULINO).size());
+		assertEquals(1, mapAlunos.get(Sexo.FEMININO).size());
+	}
+
+	@Test
+	public void deveParticionarAlunosAprovadosEReprovados() {
+		Map<Boolean, List<Aluno>> particionamento = turma.particionarAlunosAprovadosEReprovados();
+
+		assertEquals(3, particionamento.get(Boolean.TRUE).size());
+	}
+
+	@Test
+	public void deveMatchAlunos() {
+		boolean temAprovados = turma.existeAlunoAprovado();
+
+		assertEquals(true, temAprovados);
+	}
+
+	@Test
+	public void deveVerificarSeTodosOsAlunosForamAprovados() {
+		boolean lPossuiAprovados = turma.todosEstaoAprovados();
+
+		assertEquals(true, lPossuiAprovados);
+	}
+
+	@Test
+	public void deveSomarAsNotasDeTodosOsAlunos() {
+		Optional<Double> total = turma.somarTodasAsNotas();
+
+		assertEquals(Double.valueOf(27.9), total.get());
+	}
+
+}
